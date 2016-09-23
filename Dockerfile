0f69ee44b27b93fcaaa0264e79ba31f537cd9474
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-09-23 09:44:37 +0200

    run tor as 'tor' user on ubuntu

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN apt-get update && apt-get install -y tor
 
 EXPOSE 9001
 
+RUN useradd -ms /bin/bash tor
+
 USER tor
 
 COPY tor_config_relay.conf /etc/tor/torrc
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 docker run -d \
 --network=tor_network \
 -v /etc/localtime:/etc/localtime:ro \
--v tor_data:/var/lib/tor \
+-v tor_data:/tor/.tor \
 --restart always \
 -p 9001:9001 \
 wlkx/docker-tor-relay
