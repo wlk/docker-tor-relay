f82067c3b154c52fa455543f4db2370d5c536053
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-09-23 10:21:08 +0200

    config tweaks

--- setup_network.sh
+++ setup_network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# Run this once to setup a separate docker network to host your tor docker container(s), don't use it for any other containers on the same host
 docker network create --driver bridge tor_network
