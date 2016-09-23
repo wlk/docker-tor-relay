3a05222158eb8ab46b8fc38eb4fa794e4a32001a
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-09-23 09:55:06 +0200

    load data from 'debian-tor' home dir

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 docker run -d \
 --network=tor_network \
 -v /etc/localtime:/etc/localtime:ro \
--v tor_data:/home/tor/.tor \
+-v tor_data:/home/debian-tor/.tor \
 --restart always \
 -p 9001:9001 \
 wlkx/docker-tor-relay
